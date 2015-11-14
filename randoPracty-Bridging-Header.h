706c9ee486d2d2c51efe8f8042f80829625c2cec
Avery Bentz <averybentz@bell.net>
2015-11-13 21:05:55 -0500

    AWS frameworks added complete

--- randoPracty-Bridging-Header.h
+++ randoPracty-Bridging-Header.h
@@ -2,4 +2,12 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//Google Maps
 #import <GoogleMaps/GoogleMaps.h>
+//AWS
+#import <AWSCore/AWSCore.h>
+#import <AWSS3/AWSS3.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSSQS/AWSSQS.h>
+#import <AWSSNS/AWSSNS.h>
+#import <AWSCognito/AWSCognito.h>
